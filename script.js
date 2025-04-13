60c55f5b9cb1adefdbb49b6b991504bbf80c3a27
hettiweb <naqib0184@gmail.com>
2025-04-13 17:47:35 +0700

    hi my gang

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+document.onkeydown = (e) => {
+  if (e.key == "F12") {
+    alert("nice try");
+    return false;
+  }
+  if (e.ctrlKey && e.key == "u") {
+    alert("nice try dek");
+    return false;
+  }
+  if (e.ctrlKey && e.key == "c") {
+    alert("dont copy paste");
+    return false;
+  }
+  if (e.ctrlKey && e.key == "v") {
+    alert("dont copy paste");
+    return false;
+  }
+};
+
 const toogleIcon = document.querySelector(".toggle-icon");
 
 toogleIcon.addEventListener("click", () => {
@@ -5,6 +24,24 @@ toogleIcon.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
 
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.screenY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+      });
+    }
+  });
+};
 window.onscroll = () => {
   let header = document.querySelector(".header");
 
